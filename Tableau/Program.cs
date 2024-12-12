f8dc6c5c58e672ce473c6aa69c4e71619de4f31f
Jean-Pierre Bouneck <jp.bouneck@stag.technofuturtic.education>
2024-12-12 21:15:43 +0100

    Exo sur les tableau

---
+++ Tableau/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+string strPattern = @"^\d+$";
+string strDisplay=string.Empty;
+int cpt = 0;
+const int MaxLimit= 6;
+int[] tab=new int[MaxLimit];
+Console.Write("Entrez un chiffre ");
+string strNumber = Console.ReadLine();
+while (cpt < tab.Length)
+{
+      
+    if (Regex.Match(strNumber, strPattern).Success)
+    {
+        tab[cpt] = int.Parse(strNumber);
+        cpt++;  
+    }
+    Console.Clear();
+    Console.Write("Entrez un chiffre ");
+    strNumber = Console.ReadLine();
+    
+}
+for(int i=0;i< tab.Length; i++)
+{
+    strDisplay += $"{tab[i]} ";
+   
+}
+Console.WriteLine($"Le tableau : {strDisplay} ");
+
+
+
